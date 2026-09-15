09e14eba96a2a92b0ef15e6923def6ade47dedfb
kojisekimoto <gcmsekimoto@gmail.com>
2026-09-16 07:01:49 +0900

    fix many parts

--- cruise0/src/main.jsx
+++ cruise0/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 import App from './App.jsx'
 
